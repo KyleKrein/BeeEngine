d27ad4fc3416d8bb276b0d5d6a759383fd3fe8b2
KyleKreinMac <ssbank1488@gmail.com>
2023-06-05 22:02:53 +0200

    FileDialogs for MacOs was implemented

---
+++ src/Engine/src/Platform/MacOS/FileDialogs.mm
@@ -0,0 +1,41 @@
+//
+// Created by alexl on 05.06.2023.
+//
+
+#if defined(MACOS)
+#include "Utils/FileDialogs.h"
+#include "Cocoa/Cocoa.h"
+
+std::optional<std::string> BeeEngine::FileDialogs::OpenFile(const char* filter)
+{
+    // Create the File Open Dialog class.
+    NSOpenPanel* openDlg = [NSOpenPanel openPanel];
+
+// Enable the selection of files in the dialog.
+    [openDlg setCanChooseFiles:YES];
+
+// Multiple files not allowed
+    [openDlg setAllowsMultipleSelection:NO];
+
+// Can't select a directory
+    [openDlg setCanChooseDirectories:NO];
+    if ([openDlg runModal] == NSModalResponseOK) {
+            NSURL *nsurl = [[openDlg URLs] objectAtIndex:0];
+            // nsurl.path contains the NSString I want to return as std::string
+            return std::string([[[nsurl absoluteURL] path] UTF8String]);
+    }
+    return std::nullopt;
+}
+
+std::optional<std::string> BeeEngine::FileDialogs::SaveFile(const char* filter)
+{
+    NSSavePanel *saveDlg = [NSSavePanel savePanel];
+    //[saveDlg setAllowedFileTypes:[NSArray arrayWithObject:@""]];
+    if ([saveDlg runModal] == NSModalResponseOK) {
+        NSString *nsurl = [[saveDlg URL] path];
+        // nsurl.path contains the NSString I want to return as std::string
+        return std::string([nsurl UTF8String]);
+    }
+    return std::nullopt;
+}
+#endif
