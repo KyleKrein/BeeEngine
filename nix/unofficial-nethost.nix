868de271e158cdd151fff8de964b92e13c21c680
Aleksandr Lebedev <alex.lebedev2003@icloud.com>
2025-03-09 15:36:20 +0100

    x86_64-linux first fixes

--- nix/unofficial-nethost.nix
+++ nix/unofficial-nethost.nix
@@ -1,8 +1,8 @@
 { pkgs, lib } :
 let 
 version = pkgs.dotnet-sdk.version;
-system = (if pkgs.system == "aarch64-linux" then "linux-arm64" else
-"unknown");
+system = (if pkgs.system == "aarch64-linux" then "linux-arm64" else (if pkgs.system == "x86_64-linux" then "linux-x64" else
+"unknown"));
 libPath = "${system}/8.0.12/runtimes/${system}/native";
 finalPath = "${pkgs.dotnet-sdk}/share/dotnet/packs/Microsoft.NETCore.App.Host.${libPath}";
 cmakefile = pkgs.writeText "CMakeLists.txt" ''
